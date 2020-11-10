5372bb56f06ebf5647d39c686764473349ab6def
idallas93 <Dallas.Isaac1@gmail.com>
2020-11-10 18:28:45 -0500

    fixed type

--- models/dog.js
+++ models/dog.js
@@ -59,7 +59,7 @@ module.exports = function(sequelize, DataTypes) {
   // Add a belongsTo association to Authors here
   // Example: https://github.com/sequelize/express-example/blob/master/models/task.js
   Dog.associate = db => {
-    Dog.belongsTo(db.Author);
+    Dog.belongsTo(db.User);
   };
   return Dog;
 };
