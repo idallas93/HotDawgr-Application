88fb181bfeb7474bdd5c8c458eece8c76443ce6a
Sahar Vacnich <saharvacnich@Sahars-MacBook-Pro.local>
2020-11-10 20:07:24 -0400

    Added submit listener for dog form

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,9 +4,22 @@ $(document).ready(() => {
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
   // Getting references to Dog Inputs
+  const dogSignupForm = $("form.signup2");
   const dogBreed = $("#dogType-input");
   const dogGender = $("input[name='gender']:checked");
   const dogNeutered = $("input[name='dogFix']:checked");
+  const dogName = $("#dogName-input");
+  const dogAge = $("#dogAge-input"); 
+  const dogColor = $("#dogFur-input");
+  const lookingFor = $("input[name='typeDate']:checked");
+  const dogBio = $("#dogBio");
+  // Getting references to human info
+  const humanSignupForm = $("form.signup3");
+  const humanGender = $("input[name='genderHuman']:checked");
+  const humanCity = $("#cityLocation-input");
+  const humanName = $("#humanName-input");
+  const humanAge = $("#humanAge-input");
+  const humanBio = $("#humanBio-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
@@ -24,6 +37,20 @@ $(document).ready(() => {
     emailInput.val("");
     passwordInput.val("");
   });
+
+  dogSignupForm.on("submit", event => {
+    event.preventDefault();
+    const dogData = {
+      breed: dogBreed.val().trim(),
+      gender: dogGender.val().trim(),
+      fixed: dogNeutered.val(),
+      dogName: dogName.val().trim(),
+      age: dogAge.val().trim(),
+      color: dogColor.val().trim(),
+      reason: lookingFor.val().trim(),
+      dogBio: dogBio.val().trim()
+    }
+  })
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
@@ -43,32 +70,4 @@ $(document).ready(() => {
     $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
-
-
-  $("#button-one").click(function (event) {
-    event.preventDefault();
-    console.log("is this working");
-    $(".signup").hide();
-    $(".signup2").show();
-  })
-
-  $("#button-two-next").click(function (event) {
-    event.preventDefault();
-    console.log("is this working");
-    $(".signup2").hide();
-    $(".signup3").show();
-  })
-
-  $("#button-two-back").click(function (event) {
-    event.preventDefault();
-    console.log("is this working");
-    $(".signup2").hide();
-    $(".signup").show();
-  })
-  $("#button-three-back").click(function (event) {
-    event.preventDefault();
-    console.log("is this working");
-    $(".signup3").hide();
-    $(".signup2").show();
-  })
 });
